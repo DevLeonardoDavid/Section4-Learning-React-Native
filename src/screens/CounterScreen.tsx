5b9f78eefb3eb2181fff3f8aa5c7e5509dd3b4fd
devLeonardoDavid <devLeonardoDavid@gmail.com>
2023-11-07 09:23:25 -0600

    Styles centralized in the same file

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,33 +1,21 @@
 import { useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 
 export const CounterScreen = () => {
 
   const [counter, setCounter] = useState(10);
 
   return (
-    <View style ={{
-      flex: 1, //Indicates to take all the available space of the parent content
-      justifyContent: 'center'
-    }}>
+    <View style ={styles.container}>
 
-      <Text style={{
-        textAlign: 'center',
-        fontSize: 40,
-        // position: 'relative', //default setting for all RN components
-        top: -15,
-      }}>
+      <Text style={ styles.title}>
         Counter: { counter }
       </Text>
 
       <TouchableOpacity
         onPress={ () => setCounter( counter + 1)}
       >
-        <View style= {{
-          backgroundColor: 'red',
-          borderRadius: 100,
-          height: 40,
-        }}>
+        <View style= { styles.increaseButton}>
           <Text>+1</Text>
         </View>
       </TouchableOpacity>
@@ -35,3 +23,22 @@ export const CounterScreen = () => {
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, //Indicates to take all the available space of the parent content
+    justifyContent: 'center'
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 40,
+    // position: 'relative', //default setting for all RN components
+    top: -15,
+  },
+  increaseButton: {
+    backgroundColor: 'red',
+    borderRadius: 100,
+    height: 40,
+  }
+});
