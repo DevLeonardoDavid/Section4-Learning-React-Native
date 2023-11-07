b268b3f7e2b58a6b9d982d09bad2fde9f8280893
devLeonardoDavid <devLeonardoDavid@gmail.com>
2023-11-07 09:33:52 -0600

    Custom Floating Action Button Created. Added substraction operation

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -14,9 +14,19 @@ export const CounterScreen = () => {
 
       <TouchableOpacity
         onPress={ () => setCounter( counter + 1)}
+        style={ styles.fabLocationBR }
       >
-        <View style= { styles.increaseButton}>
-          <Text>+1</Text>
+        <View style={ styles.fab }>
+          <Text style={ styles.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={ () => setCounter( counter - 1)}
+        style={ styles.fabLocationBL }
+      >
+        <View style={ styles.fab }>
+          <Text style={ styles.fabText}>-1</Text>
         </View>
       </TouchableOpacity>
 
@@ -31,14 +41,32 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   title: {
-    textAlign: 'center',
-    fontSize: 40,
     // position: 'relative', //default setting for all RN components
+    fontSize: 40,
+    textAlign: 'center',
     top: -15,
   },
-  increaseButton: {
-    backgroundColor: 'red',
+  fabLocationBR: {//Bottom Right
+    position: 'absolute', //Fijo en la posición inicial donde se creó
+    bottom: 25, //Basado en el padre, ponlo al final
+    right: 25,
+  },
+  fabLocationBL: {//Bottom Right
+    position: 'absolute', 
+    bottom: 25,
+    left: 25,
+  },
+  fab: { //FloatingActionButton
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
     borderRadius: 100,
-    height: 40,
+    justifyContent: 'center',
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
   }
 });
